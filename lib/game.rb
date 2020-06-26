295191fb86c2763dc503e29788d9d1aa459cab07
Eduardo Parra <helloeduardo@icloud.com>
2020-06-25 20:30:02 -0600

    Add outline for start method

--- lib/game.rb
+++ lib/game.rb
@@ -5,6 +5,7 @@ class Game
   def initialize
     @secret_word = get_secret_word
     @guessed_word = blank_secret_word
+    @guesses_left = 12
   end
 
   def get_secret_word
@@ -17,6 +18,32 @@ class Game
 
   def blank_secret_word
     secret_word.map { |char|  "_" }
+  end
+
+  def start
+
+
+    until over?
+      #display
+      #ask
+      guess = gets.chomp
+      if guess.invalid?
+        puts "invalid, try again"
+        next
+      end
+      #update, true if guess matches
+    end
+
+  end
+
+  def over?
+    true
+  end
+
+  def display
+    puts "You have #{guesses_left} of guesses left"
+    #display array of incorrect letters
+    #display board with correct letters filled in
   end
 
 end
